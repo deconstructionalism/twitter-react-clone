9ac013c8a8dc960bd2b985713528970975b33813
deconstructionalism <deconstructionalism@gmail.com>
2018-08-16 15:14:56 -0400

    Rendered Tweet list and added some styles.
    Each tweet now shows data, also, we used spread operator to unpack props
    from tweetData to send into each Tweet Component. It's not ugly...

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import '../styles/Tweet.css'
 
-const Tweet = () => {
+const Tweet = props => {
   return (
-    <div>
-      
+    <div className="Tweet-div">
+      <div className="Tweet-header">
+        <span className="Tweet-author">{props.author}</span>
+        <span className="Tweet-date">{props.createdDate}</span>
+      </div>
+      <p className="Tweet-text">{props.text}</p>
     </div>
   )
 }
--- src/components/TweetList.js
+++ src/components/TweetList.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import '../styles/TweetList.css'
 
 import Tweet from './Tweet.js'
 
 const TweetList = props => {
 
   const Tweets = props.tweets.map((tweetData, index) => {
-    return <Tweet key={index} tweetData={tweetData}/>
+    // return <Tweet key={index} tweetData={tweetData}/>
+    return (
+      <Tweet key={index} 
+             {...tweetData}/>
+    )
   })
   return (
-    <div>
+    <div className="TweetList-div">
       {Tweets}
     </div>
   )
